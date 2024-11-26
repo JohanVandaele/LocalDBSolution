790553874e9e7449e7703eda081d7f598d4b07bb
Admin <Admin@DESKTOP-5018TGD>
2024-11-26 15:33:07 +0100

    Add project files.

---
+++ LocalDB/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+try
+{
+    var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Database.mdf;Integrated Security=True";
+    
+    using var connection = new SqlConnection(connectionString);
+
+    var comKlanten = connection.CreateCommand();
+    comKlanten.CommandType = CommandType.Text;
+    comKlanten.CommandText = "select Voornaam, Familienaam from klanten";
+
+    connection.Open();
+    Console.WriteLine("Database geopend");
+
+    using var rdrKlanten = comKlanten.ExecuteReader();
+
+    var klantFamilienaamPos = rdrKlanten.GetOrdinal("Familienaam");
+    var klantVoornaamPos = rdrKlanten.GetOrdinal("Voornaam");
+
+    while (rdrKlanten.Read())
+        Console.WriteLine($"{rdrKlanten.GetString(klantVoornaamPos)} {rdrKlanten.GetString(klantFamilienaamPos)}");
+
+    Console.WriteLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"{ex.Message}");
+}
